3201e0b022af0c0e38d0a2eb2d2df4fef753f9a4
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2023-02-15 23:07:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const http = require("http");
 const express = require("express");
-const {Server} = require("socket.io");
 const port = process.env.PORT || 4000;
 const path = require("path");
 const pdp = path.join(__dirname,"./build");
 const app = express();
-const cors = require("cors");
-const fs = require("fs");
 app.use(express.static(pdp));
 const server = http.createServer(app);
 
